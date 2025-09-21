01f693b302810c22062e13668ec383c8e7eb9540
이준태 <esse3404028@naver.com>
2025-09-21 16:43:40 +0900

    chore: 프로젝트 세팅

---
+++ app/src/main/java/com/pappang/poppang_aos/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.pappang.poppang_aos.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val kakao = Color(0xFFFEE500)
+val apple = Color(0xFF1B1B1B)
+
+val mainOrange = Color(0xFFFF7A00)
+val mainRed = Color(0xFFDD0000)
+val mainGreen = Color(0xFF006813)
+val mainWhite = Color(0xFFF1F1F1)
+val mainBlack = Color(0xFF333333)
+
+val mainGray1 = Color(0xFF777777)
+val mainGray2 = Color(0xFFAAAAAA)
+val mainGray3 = Color(0xFFCCCCCC)
+val mainGray4 = Color(0xFFF8F8F8)
