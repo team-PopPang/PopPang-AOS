390b2298eb720ae9da4c9875c973b343891dbfc6
이준태 <esse3404028@naver.com>
2025-09-22 14:17:15 +0900

    chore: Gradle 버전 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.pappang.poppang_aos"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.pappang.poppang_aos"
-        minSdk = 35
-        targetSdk = 35
+        minSdk = 30
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
