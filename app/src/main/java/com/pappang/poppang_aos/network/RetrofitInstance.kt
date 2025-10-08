4ec79d555724b62e63ae28c2d16b9dae9a91261a
이준태 <esse3404028@naver.com>
2025-10-09 01:55:48 +0900

    feat: 서버연동 추가

---
+++ app/src/main/java/com/pappang/poppang_aos/network/RetrofitInstance.kt
@@ -0,0 +1,34 @@
+package com.pappang.poppang_aos.network
+
+import com.pappang.poppang_aos.BuildConfig
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitInstance {
+    private const val AUTH_BASE_URL = BuildConfig.AUTH_BASE_URL
+    private const val USER_BASE_URL = BuildConfig.USER_BASE_URL
+
+    val authApi: AuthApi by lazy {
+        Retrofit.Builder()
+            .baseUrl(AUTH_BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(AuthApi::class.java)
+    }
+
+    val userSignUpApi: UserSignUpApi by lazy {
+        Retrofit.Builder()
+            .baseUrl(AUTH_BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(UserSignUpApi::class.java)
+    }
+
+    val duplicateNicknameApi: DuplicateNicknameApi by lazy {
+        Retrofit.Builder()
+            .baseUrl(USER_BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(DuplicateNicknameApi::class.java)
+    }
+}
