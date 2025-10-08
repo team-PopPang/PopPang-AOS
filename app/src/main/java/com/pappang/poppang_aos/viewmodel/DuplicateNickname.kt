4ba49d65d4c7d6a33d1c745a01cb760006d948ce
이준태 <esse3404028@naver.com>
2025-10-09 01:54:48 +0900

    feat: 닉네임 중복여부 확인 추가

--- app/src/main/java/com/pappang/poppang_aos/viewmodel/DuplicateNickname.kt
+++ app/src/main/java/com/pappang/poppang_aos/viewmodel/DuplicateNickname.kt
@@ -4,6 +4,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.pappang.poppang_aos.network.RetrofitInstance
+import kotlinx.coroutines.launch
 
 class DuplicateNickname : ViewModel() {
     var nickname by mutableStateOf("")
@@ -15,6 +18,8 @@ class DuplicateNickname : ViewModel() {
     var isSuccess by mutableStateOf(false)
         private set
 
+    private val duplicateNicknameApi = RetrofitInstance.duplicateNicknameApi
+
     fun onNicknameChange(newNickname: String) {
         nickname = newNickname
         checkResult = ""
@@ -23,18 +28,36 @@ class DuplicateNickname : ViewModel() {
     }
 
     fun checkNickname() {
-        if (nickname.trim() == "admin") {
-            checkResult = "이미 사용 중인 닉네임입니다"
+        if (nickname.isBlank()) {
+            checkResult = "닉네임을 입력해주세요."
             isError = true
             isSuccess = false
-        } else if (nickname.isNotBlank()) {
-            checkResult = "사용 가능한 닉네임입니다"
-            isError = false
-            isSuccess = true
-        } else {
-            checkResult = ""
-            isError = false
-            isSuccess = false
+            return
+        }
+        viewModelScope.launch {
+            try {
+                val response = duplicateNicknameApi.checkNickname(nickname)
+                if (response.isSuccessful && response.body() != null) {
+                    val isDuplicated = response.body()!!.isDuplicated
+                    if (isDuplicated) {
+                        checkResult = "이미 사용 중인 닉네임입니다"
+                        isError = true
+                        isSuccess = false
+                    } else {
+                        checkResult = "사용 가능한 닉네임입니다"
+                        isError = false
+                        isSuccess = true
+                    }
+                } else {
+                    checkResult = "서버 오류"
+                    isError = true
+                    isSuccess = false
+                }
+            } catch (e: Exception) {
+                checkResult = "네트워크 오류"
+                isError = true
+                isSuccess = false
+            }
         }
     }
 }
