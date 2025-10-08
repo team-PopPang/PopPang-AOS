26de8e0957e8de2faf2108e25db41e08de009209
이준태 <esse3404028@naver.com>
2025-10-09 01:56:59 +0900

    chore: 의존성 부여

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,4 @@
-
+import java.util.Properties
 
 plugins {
     alias(libs.plugins.android.application)
@@ -18,6 +18,30 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        val localProperties = Properties()
+        localProperties.load(project.rootProject.file("local.properties").inputStream())
+        val kakaoKey = localProperties.getProperty("KAKAO_KEY") ?: ""
+        val googleKey = localProperties.getProperty("GOOGLE_KEY") ?: ""
+        val authurl = localProperties.getProperty("AUTH_BASE_URL") ?: ""
+        val userurl = localProperties.getProperty("USER_BASE_URL") ?: ""
+        val authapikakao = localProperties.getProperty("AUTH_API_KAKAO") ?: ""
+        val authapigoogle = localProperties.getProperty("AUTH_API_GOOGLE") ?: ""
+        val authapiautologin = localProperties.getProperty("AUTH_API_AUTOLOGIN") ?: ""
+        val duplicateapi = localProperties.getProperty("DUPLICATE_API") ?: ""
+        val signupapikakao = localProperties.getProperty("SIGNUP_API_KAKAO") ?: ""
+        val signupapigoogle = localProperties.getProperty("SIGNUP_API_GOOGLE") ?:
+        buildConfigField("String", "KAKAO_KEY", "\"$kakaoKey\"")
+        buildConfigField("String", "GOOGLE_KEY", "\"$googleKey\"")
+        manifestPlaceholders["KAKAO_KEY"] = kakaoKey
+        buildConfigField("String", "AUTH_BASE_URL", "\"$authurl\"")
+        buildConfigField("String", "USER_BASE_URL", "\"$userurl\"")
+        buildConfigField("String", "AUTH_API_KAKAO", "\"$authapikakao\"")
+        buildConfigField("String", "AUTH_API_GOOGLE", "\"$authapigoogle\"")
+        buildConfigField("String", "AUTH_API_AUTOLOGIN", "\"$authapiautologin\"")
+        buildConfigField("String", "DUPLICATE_API", "\"$duplicateapi\"")
+        buildConfigField("String", "SIGNUP_API_KAKAO", "\"$signupapikakao\"")
+        buildConfigField("String", "SIGNUP_API_GOOGLE", "\"$signupapigoogle\"")
     }
 
     buildTypes {
@@ -38,6 +62,7 @@ android {
     }
     buildFeatures {
         compose = true
+        buildConfig = true
     }
 }
 
@@ -52,6 +77,12 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation(libs.v2.user)
+    implementation(libs.play.services.auth)
+    implementation(libs.google.services)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
