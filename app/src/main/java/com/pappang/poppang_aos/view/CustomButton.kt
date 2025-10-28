acbeb9bb1fd971ad332305fb5f3b5319d9f6f485
이준태 <esse3404028@naver.com>
2025-10-28 21:56:45 +0900

    feat: 커스텀 버튼 추가

--- app/src/main/java/com/pappang/poppang_aos/view/CustomButton.kt
+++ app/src/main/java/com/pappang/poppang_aos/view/CustomButton.kt
@@ -3,7 +3,9 @@ package com.pappang.poppang_aos.view
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -11,12 +13,16 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment.Companion.Center
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.pappang.poppang_aos.ui.theme.Medium12
 import com.pappang.poppang_aos.ui.theme.Medium15
+import com.pappang.poppang_aos.ui.theme.Regular9
+import com.pappang.poppang_aos.ui.theme.mainBlack
+import com.pappang.poppang_aos.ui.theme.mainGray5
 import com.pappang.poppang_aos.ui.theme.mainOrange
 
 @Composable
@@ -96,14 +102,41 @@ fun CustomButton4(onClick: () -> Unit, text: String, modifier: Modifier) {
     }
 }
 
+@Composable
+fun CustomButton5(onClick: () -> Unit, text: String, modifier: Modifier) {
+    Box(
+        modifier = modifier
+            .background(Color.White, shape = RoundedCornerShape(20.dp))
+            .height(30.dp)
+            .border(width = 1.dp, color = mainGray5, shape = RoundedCornerShape(20.dp))
+    ) {
+        Row(
+            modifier = modifier
+                .align(Center),
+            verticalAlignment = CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ) {
+            Text(
+                text = text,
+                style = Regular9,
+                color = mainBlack,
+                modifier = modifier
+                    .padding(horizontal = 10.dp, vertical = 8.dp)
+            )
+            Text(
+                text = "✕",
+                style = Regular9,
+                color = mainBlack,
+                modifier = modifier
+                    .clickable { onClick()}
+                    .padding(end = 10.dp, top = 8.dp, bottom = 8.dp)
+            )
+        }
+    }
+}
 
 @Composable
 @Preview
-fun CustomButtonPreview() {
-    CustomButton3(onClick = {}, text = "커스텀 버튼", modifier = Modifier)
-}
-@Composable
-@Preview
-fun CustomButton2Preview() {
-    CustomButton4(onClick = {}, text = "커스텀 버튼2", modifier = Modifier)
+fun CustomButton5Preview() {
+    CustomButton5(onClick = {}, text = "취소", modifier = Modifier)
 }
