828c14b930c4ff23f6e47868aa6e0e56843f4e11
이준태 <esse3404028@naver.com>
2025-10-28 21:57:58 +0900

    docs: 글꼴 수정

--- app/src/main/java/com/pappang/poppang_aos/ui/theme/Type.kt
+++ app/src/main/java/com/pappang/poppang_aos/ui/theme/Type.kt
@@ -12,11 +12,15 @@ val ExtraBold24 = TextStyle(
     fontFamily = FontFamily(Font(R.font.scdream7)),
     //fontWeight = FontWeight.ExtraBold,
     fontSize = 24.sp,
+    letterSpacing = 0.48.sp,
+    lineHeight = 33.6.sp
 )
 val ExtraBold18 = TextStyle(
     fontFamily = FontFamily(Font(R.font.scdream7)),
     //fontWeight = FontWeight.ExtraBold,
     fontSize = 18.sp,
+    letterSpacing = 0.36.sp,
+    lineHeight = 25.2.sp
 )
 
 //Bold
@@ -25,21 +29,29 @@ val Bold17 = TextStyle(
     fontFamily = FontFamily(Font(R.font.scdream6)),
     //fontWeight = FontWeight.Bold,
     fontSize = 17.sp,
+    letterSpacing = 0.34.sp,
+    lineHeight = 23.8.sp
 )
 val Bold12 = TextStyle(
     fontFamily = FontFamily(Font(R.font.scdream6)),
     //fontWeight = FontWeight.Bold,
     fontSize = 12.sp,
+    letterSpacing = 0.24.sp,
+    lineHeight = 16.8.sp
 )
 val Bold15 = TextStyle(
     fontFamily = FontFamily(Font(R.font.scdream6)),
     //fontWeight = FontWeight.Bold,
     fontSize = 15.sp,
+    letterSpacing = 0.3.sp,
+    lineHeight = 21.sp
 )
 val Bold10 = TextStyle(
     fontFamily = FontFamily(Font(R.font.scdream6)),
     //fontWeight = FontWeight.Bold,
     fontSize = 10.sp,
+    letterSpacing = 0.2.sp,
+    lineHeight = 14.sp
 )
 
 //Medium
@@ -47,44 +59,60 @@ val Medium18 = TextStyle(
     fontFamily = FontFamily(Font(R.font.scdream5)),
     //fontWeight = FontWeight.Medium,
     fontSize = 18.sp,
+    letterSpacing = 0.36.sp,
+    lineHeight = 25.2.sp
 )
 val Medium17 = TextStyle(
     fontFamily = FontFamily(Font(R.font.scdream5)),
     //fontWeight = FontWeight.Medium,
     fontSize = 17.sp,
+    letterSpacing = 0.34.sp,
+    lineHeight = 23.8.sp
 )
 val Medium15 = TextStyle(
     fontFamily = FontFamily(Font(R.font.scdream5)),
     //fontWeight = FontWeight.Medium,
     fontSize = 15.sp,
+    letterSpacing = 0.3.sp,
+    lineHeight = 21.sp
 )
 val Medium13 = TextStyle(
     fontFamily = FontFamily(Font(R.font.scdream5)),
     //fontWeight = FontWeight.Medium,
     fontSize = 13.sp,
+    letterSpacing = 0.26.sp,
+    lineHeight = 18.2.sp
 )
 
 val Medium12 = TextStyle(
     fontFamily = FontFamily(Font(R.font.scdream5)),
     //fontWeight = FontWeight.Medium,
     fontSize = 12.sp,
+    letterSpacing = 0.24.sp,
+    lineHeight = 16.8.sp
 )
-val Medium10 = TextStyle(
+val Medium11 = TextStyle(
     fontFamily = FontFamily(Font(R.font.scdream5)),
     //fontWeight = FontWeight.Medium,
-    fontSize = 10.sp,
+    fontSize = 11.sp,
+    letterSpacing = 0.22.sp,
+    lineHeight = 15.4.sp
 )
 
 val Medium8 = TextStyle(
     fontFamily = FontFamily(Font(R.font.scdream5)),
     //fontWeight = FontWeight.Medium,
     fontSize = 8.sp,
+    letterSpacing = 0.16.sp,
+    lineHeight = 11.2.sp
 )
 
 val Medium7 = TextStyle(
     fontFamily = FontFamily(Font(R.font.scdream5)),
     //fontWeight = FontWeight.Medium,
     fontSize = 7.sp,
+    letterSpacing = 0.14.sp,
+    lineHeight = 9.8.sp
 )
 
 //Regular
@@ -93,21 +121,29 @@ val Regular15 = TextStyle(
     fontFamily = FontFamily(Font(R.font.scdream4)),
     //fontWeight = FontWeight.Regular,
     fontSize = 15.sp,
+    letterSpacing = 0.3.sp,
+    lineHeight = 21.sp
 )
 val Regular12 = TextStyle(
     fontFamily = FontFamily(Font(R.font.scdream4)),
     //fontWeight = FontWeight.Regular,
     fontSize = 12.sp,
+    letterSpacing = 0.24.sp,
+    lineHeight = 16.8.sp
 )
 val Regular11 = TextStyle(
     fontFamily = FontFamily(Font(R.font.scdream4)),
     //fontWeight = FontWeight.Regular,
     fontSize = 11.sp,
+    letterSpacing = 0.22.sp,
+    lineHeight = 15.4.sp
 )
 val Regular9 = TextStyle(
     fontFamily = FontFamily(Font(R.font.scdream4)),
     //fontWeight = FontWeight.Regular,
     fontSize = 9.sp,
+    letterSpacing = 0.18.sp,
+    lineHeight = 12.6.sp
 )
 
 //Light
@@ -115,6 +151,8 @@ val Light10 = TextStyle(
     fontFamily = FontFamily(Font(R.font.scdream3)),
     //fontWeight = FontWeight.Light,
     fontSize = 10.sp,
+    letterSpacing = 0.2.sp,
+    lineHeight = 14.sp
 )
 
 
